a8a8175911dac9be83e96ec250c71b34dadaa30a
O.Sedniev <o.sedniev@gmail.com>
2021-03-23 12:09:33 +0200

    commit

--- google-drive/src/app/app.component.ts
+++ google-drive/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-// import { GoogleAuthService } from 'ng-gapi';
+import { GoogleAuthService } from 'ng-gapi';
+import { HttpClient, HttpHeaders, HttpParams, } from '@angular/common/http';
 declare let gapi:any;
-
+import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,58 +10,92 @@ declare let gapi:any;
 })
 export class AppComponent {
   title = 'google-drive';
-  public gapiSetup: boolean = false; // marks if the gapi library has been loaded
-  public authInstance: any;
-  public error: string;
-  public user: any;
-
-  // constructor(private googleAuth: GoogleAuthService){ 
-  // }
+  token
+  content
+  constructor(private googleAuth: GoogleAuthService,
+              private http: HttpClient){ 
+  }
   
   auth():void{
     console.log('auth')
-    // this.signIn();
-    this.authenticate();
+    this.signIn();
+    // this.authenticate();
   }
 
-  // public signIn(): void {
-  //   this.googleAuth.getAuth()
-  //       .subscribe((auth) => {
-  //           console.log(auth)
+  public signIn(): void {
+    this.googleAuth.getAuth()
+        .subscribe((auth) => {
+            auth.signIn().then(res => {
+              console.log(res.getAuthResponse().access_token);
+              this.token = res.getAuthResponse().access_token;
+              console.log(this.token);
+            });
+        });
+  }
+
+  createFile(): void{
+      this.postRequest('https://www.googleapis.com/upload/drive/v3/files',this.content).subscribe(
+        (resp)=>{
+          console.log(resp);
+        }
+      );
+  }
+
+  public postRequest(url:string, headerData: any[] = [], bodyData = {}) {
+    let headers = new HttpHeaders({
+      'Content-Type': 'text/xml',
+      'Authorization': 'Bearer ' + this.token,
+      'name': "test_name"
+    });
+
+    return this.http.post(url, bodyData, { headers: headers });
+}
+
+
+
+  dropped(files: NgxFileDropEntry[]): void{
+    if(files.length>1){
+    }else{
+      if(files[0].fileEntry.isFile){
+        // this.content = files;
+        const fileEntry = files[0].fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File)=>{
+          let reader:FileReader = new FileReader();
+          reader.readAsText(file, "windows-1251");
+          reader.onloadend = function(){
+            this.content = reader.result;
+            console.log("file drop");
+          }.bind(this);  
+        })  
+      }
+    }
+  }
+  
+  // async initGoogleAuth(): Promise<void> {
+  //   const pload = new Promise((resolve) => {
+  //     gapi.load('auth2', resolve);
+  //   });
+
+  //   return pload.then(async () => {
+  //     await gapi.auth2
+  //       .init({ client_id: '223971077227-mfpp3qa0tlfs2mctcfhum5h4tbtv7aul.apps.googleusercontent.com' })
+  //       .then(auth => {
+  //         this.gapiSetup = true;
+  //         this.authInstance = auth;
+  //         console.log(auth);
   //       });
+  //   });
   // }
 
-  async initGoogleAuth(): Promise<void> {
-    //  Create a new Promise where the resolve 
-    // function is the callback passed to gapi.load
-    const pload = new Promise((resolve) => {
-      gapi.load('auth2', resolve);
-    });
-
-    // When the first promise resolves, it means we have gapi
-    // loaded and that we can call gapi.init
-    return pload.then(async () => {
-      await gapi.auth2
-        .init({ client_id: '223971077227-mfpp3qa0tlfs2mctcfhum5h4tbtv7aul.apps.googleusercontent.com' })
-        .then(auth => {
-          this.gapiSetup = true;
-          this.authInstance = auth;
-        });
-    });
-  }
-
-  async authenticate(): Promise<any> {
-    // Initialize gapi if not done yet
-    if (!this.gapiSetup) {
-      await this.initGoogleAuth();
-    }
-
-    // Resolve or reject signin Promise
-    return new Promise(async () => {
-      await this.authInstance.signIn().then(
-        user => this.user = user,
-        error => this.error = error);
-    });
-  }
+  // async authenticate(): Promise<any> {
+  //   if (!this.gapiSetup) {
+  //     await this.initGoogleAuth();
+  //   }
+  //   return new Promise(async () => {
+  //     await this.authInstance.signIn().then(
+  //       user => this.user = user,
+  //       error => this.error = error);
+  //   });
+  // }
 
 }
--- google-drive/src/app/app.module.ts
+++ google-drive/src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
-// import {
-//   GoogleApiModule, 
-//   GoogleApiService, 
-//   GoogleAuthService, 
-//   NgGapiClientConfig, 
-//   NG_GAPI_CONFIG,
-//   GoogleApiConfig
-// } from "ng-gapi";
+import {
+  GoogleApiModule, 
+  GoogleApiService, 
+  GoogleAuthService, 
+  NgGapiClientConfig, 
+  NG_GAPI_CONFIG,
+  GoogleApiConfig
+} from "ng-gapi";
 import { TestComponent } from './test/test.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
-// let gapiClientConfig: NgGapiClientConfig = {
-//   client_id: "223971077227-mfpp3qa0tlfs2mctcfhum5h4tbtv7aul.apps.googleusercontent.com",
-//   discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
-//   scope: [
-//       "https://www.googleapis.com/auth/drive"
-//   ].join(" ")
-// };
+let gapiClientConfig: NgGapiClientConfig = {
+  client_id: "223971077227-mfpp3qa0tlfs2mctcfhum5h4tbtv7aul.apps.googleusercontent.com",
+  discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+  scope: [
+      "https://www.googleapis.com/auth/drive"
+  ].join(" ")
+};
 
 const routes: Routes = []
 @NgModule({
@@ -29,14 +31,18 @@ const routes: Routes = []
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes),
-    // GoogleApiModule.forRoot({
-    //   provide: NG_GAPI_CONFIG,
-    //   useValue: gapiClientConfig
-    // }),
-    AppRoutingModule
+    GoogleApiModule.forRoot({
+      provide: NG_GAPI_CONFIG,
+      useValue: gapiClientConfig
+    }),
+    AppRoutingModule,
+    NgxFileDropModule
   ],
-  providers: [],
+  providers: [
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
